4630594d5a6f554bf097625ffe8d1b8032cf66bf
Dawid Sroka <dawid190393@gmail.com>
2020-10-27 23:59:28 +0100

    Add daily GenerateForecastErrors unit tests

--- AirlyAnalyzer.Tests/AuxiliaryMethodsTests/GenerateForecastErrorsTest.cs
+++ AirlyAnalyzer.Tests/AuxiliaryMethodsTests/GenerateForecastErrorsTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using AirlyAnalyzer.Models;
 using AirlyAnalyzer.Tests.Models;
 using Xunit;
 
@@ -94,6 +95,76 @@ namespace AirlyAnalyzer.Tests
 
       // Assert
       Assert.Equal(endRequestDate.ToLocalTime(), forecastErrors.Last().RequestDateTime, new TimeSpan(0, 0, 0));
+    }
+
+    /* Tests for daily forecast errors */
+
+    [Theory]
+    [InlineData(22)]
+    [InlineData(23)]
+    [InlineData(24)]
+    public void Return_correct_list_of_daily_forecast_errors(short numberOfHourlyErrorsInDay)
+    {
+      // Arrange
+      const int numberOfDailyErrors = 15;
+      const ForecastErrorType errorType = ForecastErrorType.Daily;
+
+      // Act
+      var dailyErrors = AuxiliaryMethods.GenerateForecastErrors(_installationId, errorType,
+        _startDate, numberOfDailyErrors, _requestMinutesOffset, numberOfHourlyErrorsInDay)
+      .ToList();
+
+      // Assert
+      Assert.Equal(numberOfDailyErrors, dailyErrors.Count);
+      Assert.Equal(_startDate.ToLocalTime(), dailyErrors[0].FromDateTime);
+      Assert.Equal(_startDate.AddHours(numberOfHourlyErrorsInDay).ToLocalTime(), dailyErrors[0].TillDateTime);
+      Assert.Equal(errorType, dailyErrors[0].ErrorType);
+    }
+
+    [Theory]
+    [InlineData(22)]
+    [InlineData(23)]
+    [InlineData(24)]
+    public void Return_correct_last_daily_forecast_error_date(short numberOfHourlyErrorsInDay)
+    {
+      // Arrange
+      const short numberOfDailyErrors = 15;
+      const ForecastErrorType errorType = ForecastErrorType.Daily;
+      var endDate = _startDate.AddDays(numberOfDailyErrors)
+                              .AddHours(numberOfHourlyErrorsInDay - 24);
+
+      // Act
+      var dailyErrors = AuxiliaryMethods.GenerateForecastErrors(_installationId, errorType, _startDate,
+        numberOfDailyErrors, _requestMinutesOffset, numberOfHourlyErrorsInDay)
+      .ToList();
+
+      // Assert
+      Assert.Equal(endDate.ToLocalTime(), dailyErrors.Last().TillDateTime, new TimeSpan(0, 0, 0));
+    }
+
+    [Theory]
+    [InlineData(15, 23, 23)]
+    [InlineData(16, 23, 24)]
+    [InlineData(15, 22, 22)]
+    [InlineData(16, 22, 24)]
+    [InlineData(15, 24, 24)]
+    [InlineData(16, 24, 24)]
+    public void Return_correct_last_daily_forecast_error_request_date(
+      short numberOfDailyErrors, short numberOfHourlyErrorsInDay, short numberOfHourlyErrorsInLastDay)
+    {
+      // Arrange
+      const ForecastErrorType errorType = ForecastErrorType.Daily;
+      var endRequestDate = _startDate.AddDays(numberOfDailyErrors)
+                              .AddHours(numberOfHourlyErrorsInLastDay - 24)
+                              .AddMinutes(_requestMinutesOffset);
+
+      // Act
+      var dailyErrors = AuxiliaryMethods.GenerateForecastErrors(_installationId, errorType, _startDate,
+        numberOfDailyErrors, _requestMinutesOffset, numberOfHourlyErrorsInDay)
+      .ToList();
+
+      // Assert
+      Assert.Equal(endRequestDate.ToLocalTime(), dailyErrors.Last().RequestDateTime, new TimeSpan(0, 0, 0));
     }
   }
 }

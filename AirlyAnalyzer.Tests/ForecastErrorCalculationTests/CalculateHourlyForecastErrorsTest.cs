03437c7fe8bdff1b0765e2e8a4567a2ca865c177
Dawid Sroka <dawid190393@gmail.com>
2020-11-21 16:50:36 +0100

    Split CalculateNewForecastErrorsTest into two classes
    Split this class into one class for testing calculating hourly forecast
    errors and one for daily forecast errors. Thanks to this change unit
    tests classes are more concise and easier to manage.

--- AirlyAnalyzer.Tests/ForecastErrorCalculationTests/CalculateHourlyForecastErrorsTest.cs
+++ AirlyAnalyzer.Tests/ForecastErrorCalculationTests/CalculateHourlyForecastErrorsTest.cs
@@ -7,12 +7,12 @@ namespace AirlyAnalyzer.Tests.ForecastErrorCalculationTests
   using AirlyAnalyzer.Tests.Models;
   using Xunit;
 
-  public class CalculateNewForecastErrorsTest
+  public class CalculateHourlyForecastErrorsTest
   {
     private readonly DateTime _startDate;
     private const byte _requestMinutesOffset = 30;
 
-    public CalculateNewForecastErrorsTest()
+    public CalculateHourlyForecastErrorsTest()
     {
       _startDate = new DateTime(2001, 3, 24, 22, 0, 0, DateTimeKind.Utc);
     }
@@ -197,91 +197,6 @@ namespace AirlyAnalyzer.Tests.ForecastErrorCalculationTests
       Assert.Equal(airlyCaqi_ForecastError, forecastErrors[0].AirlyCaqiPctError);
       Assert.Equal(airlyPm25_ForecastError, forecastErrors[0].Pm25PctError);
       Assert.Equal(airlyPm10_ForecastError, forecastErrors[0].Pm10PctError);
-    }
-
-    /* Tests for daily forecast errors */
-
-    [Theory]
-    [InlineData(0, 0, 22, 0)]
-    [InlineData(1, 21, 22, 0)]
-    [InlineData(1, 22, 22, 1)]
-    [InlineData(1, 23, 22, 1)]
-    [InlineData(5, 21, 22, 0)]
-    [InlineData(5, 22, 22, 5)]
-    public void correct_number_of_daily_forecast_errors(
-      short numberOfDays,
-      short numberOfForecastErrorsInDay,
-      short minNumberOfMeasurements,
-      short numberOfDailyForecastsErrors)
-    {
-      // Arrange
-      const short installationId = 1;
-      var startDate = _startDate;
-
-      var newForecastErrors = AuxiliaryMethods.GenerateHourlyForecastErrors(
-          installationId, startDate, numberOfDays, numberOfForecastErrorsInDay, _requestMinutesOffset)
-        .ToList();
-
-      var forecastErrorsCalculation = new ForecastErrorsCalculation(minNumberOfMeasurements);
-
-      // Act
-      var dailyForecastErrors = forecastErrorsCalculation
-        .CalculateDailyForecastErrors(installationId, newForecastErrors);
-
-      // Assert
-      Assert.Equal(numberOfDailyForecastsErrors, dailyForecastErrors.Count);
-    }
-
-    [Fact]
-    public void no_daily_forecast_error_when_number_of_elements_less_than_min()
-    {
-      // Arrange
-      const short installationId = 1;
-      const short minNumberOfMeasurements = 20;
-      const short numberOfForecastErrors = 19;
-      var startDate = _startDate;
-
-      var newForecastErrors = AuxiliaryMethods.GenerateHourlyForecastErrors(
-          installationId, startDate, numberOfForecastErrors, _requestMinutesOffset)
-        .ToList();
-
-      var forecastErrorsCalculation = new ForecastErrorsCalculation(minNumberOfMeasurements);
-
-      // Act
-      var dailyForecastErrors = forecastErrorsCalculation
-        .CalculateDailyForecastErrors(installationId, newForecastErrors);
-
-      // Assert
-      Assert.Empty(dailyForecastErrors);
-    }
-
-    [Fact]
-    public void correct_daily_forecast_error_when_number_of_elements_is_equal_min()
-    {
-      // Arrange
-      const short installationId = 1;
-      const short minNumberOfMeasurements = 20;
-      const short numberOfForecastErrors = 20;
-      var startDate = _startDate;
-      var endDate = _startDate.AddHours(numberOfForecastErrors);
-
-      var newForecastErrors = AuxiliaryMethods.GenerateHourlyForecastErrors(
-          installationId, startDate, numberOfForecastErrors, _requestMinutesOffset)
-        .ToList();
-
-      var forecastErrorsCalculation = new ForecastErrorsCalculation(minNumberOfMeasurements);
-
-      // Act
-      var dailyForecastErrors = forecastErrorsCalculation
-        .CalculateDailyForecastErrors(installationId, newForecastErrors);
-
-      // Assert
-      startDate = _startDate.ToLocalTime();
-      endDate = endDate.ToLocalTime();
-
-      Assert.Single(dailyForecastErrors);
-      Assert.Equal(startDate, dailyForecastErrors[0].FromDateTime, new TimeSpan(0, 0, 0));
-      Assert.Equal(endDate, dailyForecastErrors[0].TillDateTime, new TimeSpan(0, 0, 0));
     }
   }
 }

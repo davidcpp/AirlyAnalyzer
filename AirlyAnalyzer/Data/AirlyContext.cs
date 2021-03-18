366e7cc126fed04f8924de5ac7d62531f0e8d34c
Dawid Sroka <dawid190393@gmail.com>
2021-03-18 21:42:35 +0100

    Extract entity configuration code to new classes

--- AirlyAnalyzer/Data/AirlyContext.cs
+++ AirlyAnalyzer/Data/AirlyContext.cs
@@ -3,8 +3,63 @@
   using AirlyAnalyzer.Models;
   using Microsoft.EntityFrameworkCore;
   using Microsoft.EntityFrameworkCore.Metadata;
+  using Microsoft.EntityFrameworkCore.Metadata.Builders;
   using Microsoft.Extensions.Configuration;
   using Microsoft.Extensions.Logging;
+
+  public class MeasurementConfiguration
+      : IEntityTypeConfiguration<AirQualityMeasurement>
+  {
+    public void Configure(EntityTypeBuilder<AirQualityMeasurement> builder)
+    {
+      builder.ToTable("ArchiveMeasurements")
+          .HasKey(x => new { x.InstallationId, x.FromDateTime, x.TillDateTime });
+
+      builder.Property(x => x.FromDateTime).HasColumnType("smalldatetime");
+      builder.Property(x => x.TillDateTime).HasColumnType("smalldatetime");
+      builder.Property(x => x.RequestDateTime).HasColumnType("smalldatetime");
+    }
+  }
+
+  public class ForecastConfiguration
+      : IEntityTypeConfiguration<AirQualityForecast>
+  {
+    public void Configure(EntityTypeBuilder<AirQualityForecast> builder)
+    {
+      builder.ToTable("ArchiveForecasts")
+          .HasKey(x => new { x.InstallationId, x.FromDateTime, x.TillDateTime });
+
+      builder.Property(x => x.FromDateTime).HasColumnType("smalldatetime");
+      builder.Property(x => x.TillDateTime).HasColumnType("smalldatetime");
+      builder.Property(x => x.RequestDateTime).HasColumnType("smalldatetime");
+    }
+  }
+
+  public class ForecastErrorConfiguration
+      : IEntityTypeConfiguration<AirQualityForecastError>
+  {
+    private readonly byte _maxErrorTypeLength;
+
+    public ForecastErrorConfiguration(byte maxErrorTypeLength)
+    {
+      _maxErrorTypeLength = maxErrorTypeLength;
+    }
+
+    public void Configure(EntityTypeBuilder<AirQualityForecastError> builder)
+    {
+      builder.ToTable("ForecastErrors")
+          .HasKey(x => new { x.InstallationId, x.FromDateTime, x.TillDateTime });
+
+      builder.Property(x => x.ErrorType)
+          .HasConversion<string>()
+          .IsUnicode(false)
+          .HasMaxLength(_maxErrorTypeLength);
+
+      builder.Property(x => x.FromDateTime).HasColumnType("smalldatetime");
+      builder.Property(x => x.TillDateTime).HasColumnType("smalldatetime");
+      builder.Property(x => x.RequestDateTime).HasColumnType("smalldatetime");
+    }
+  }
 
   public class AirlyContext : DbContext
   {
@@ -20,8 +75,8 @@
           config.GetValue<byte>("AppSettings:Databases:MaxErrorTypeLength");
     }
 
-    public DbSet<AirQualityForecast> ArchiveForecasts { get; set; }
     public DbSet<AirQualityMeasurement> ArchiveMeasurements { get; set; }
+    public DbSet<AirQualityForecast> ArchiveForecasts { get; set; }
     public DbSet<AirQualityForecastError> ForecastErrors { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -35,59 +90,10 @@
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-      modelBuilder.Entity<AirQualityForecast>()
-        .ToTable("ArchiveForecasts")
-        .HasKey(x => new { x.InstallationId, x.FromDateTime, x.TillDateTime });
-
-      modelBuilder.Entity<AirQualityMeasurement>()
-        .ToTable("ArchiveMeasurements")
-        .HasKey(x => new { x.InstallationId, x.FromDateTime, x.TillDateTime });
-
-      modelBuilder.Entity<AirQualityForecastError>()
-        .ToTable("ForecastErrors")
-        .HasKey(x => new { x.InstallationId, x.FromDateTime, x.TillDateTime });
-
-      modelBuilder.Entity<AirQualityForecastError>()
-        .Property(x => x.ErrorType)
-        .HasConversion<string>()
-        .IsUnicode(false)
-        .HasMaxLength(_maxErrorTypeLength);
-
-      modelBuilder.Entity<AirQualityForecast>()
-        .Property(x => x.FromDateTime)
-        .HasColumnType("smalldatetime");
-
-      modelBuilder.Entity<AirQualityForecast>()
-        .Property(x => x.TillDateTime)
-        .HasColumnType("smalldatetime");
-
-      modelBuilder.Entity<AirQualityForecast>()
-        .Property(x => x.RequestDateTime)
-        .HasColumnType("smalldatetime");
-
-      modelBuilder.Entity<AirQualityMeasurement>()
-        .Property(x => x.FromDateTime)
-        .HasColumnType("smalldatetime");
-
-      modelBuilder.Entity<AirQualityMeasurement>()
-        .Property(x => x.TillDateTime)
-        .HasColumnType("smalldatetime");
-
-      modelBuilder.Entity<AirQualityMeasurement>()
-        .Property(x => x.RequestDateTime)
-        .HasColumnType("smalldatetime");
-
-      modelBuilder.Entity<AirQualityForecastError>()
-        .Property(x => x.FromDateTime)
-        .HasColumnType("smalldatetime");
-
-      modelBuilder.Entity<AirQualityForecastError>()
-        .Property(x => x.TillDateTime)
-        .HasColumnType("smalldatetime");
-
-      modelBuilder.Entity<AirQualityForecastError>()
-        .Property(x => x.RequestDateTime)
-        .HasColumnType("smalldatetime");
+      modelBuilder.ApplyConfiguration(new MeasurementConfiguration());
+      modelBuilder.ApplyConfiguration(new ForecastConfiguration());
+      modelBuilder.ApplyConfiguration(
+          new ForecastErrorConfiguration(_maxErrorTypeLength));
 
       modelBuilder.HasAnnotation(
           "SqlServer:ValueGenerationStrategy",

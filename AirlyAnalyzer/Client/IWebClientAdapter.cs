21b0b98e7931552cdf19fe365afe4e15b527ca69
Dawid Sroka <dawid190393@gmail.com>
2021-03-26 23:56:38 +0100

    Add WebClientAdapter classes to tests

---
+++ AirlyAnalyzer/Client/IWebClientAdapter.cs
@@ -0,0 +1,14 @@
+﻿namespace AirlyAnalyzer.Client
+{
+  using System.Net;
+  using System.Threading.Tasks;
+
+  public interface IWebClientAdapter
+  {
+    string BaseAddress { get; set; }
+
+    WebHeaderCollection Headers { get; set; }
+
+    Task<string> DownloadStringTaskAsync(string address);
+  }
+}
---
+++ AirlyAnalyzer/Client/WebClientAdapter.cs
@@ -0,0 +1,32 @@
+﻿namespace AirlyAnalyzer.Client
+{
+  using System.Net;
+  using System.Threading.Tasks;
+
+  public class WebClientAdapter : IWebClientAdapter
+  {
+    private readonly WebClient _webClient;
+
+    public WebClientAdapter(WebClient webClient)
+    {
+      _webClient = webClient;
+    }
+
+    public string BaseAddress
+    {
+      get => _webClient.BaseAddress;
+      set => _webClient.BaseAddress = value;
+    }
+
+    public WebHeaderCollection Headers
+    {
+      get => _webClient.Headers;
+      set => _webClient.Headers = value;
+    }
+
+    public Task<string> DownloadStringTaskAsync(string address)
+    {
+      return _webClient.DownloadStringTaskAsync(address);
+    }
+  }
+}

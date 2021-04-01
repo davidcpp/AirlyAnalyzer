46350a425dffcb5258c9f894cbe2f5d2c6322e07
Dawid Sroka <dawid190393@gmail.com>
2021-04-01 20:28:50 +0200

    Remove redundant GetRequestDates Integration Test

--- AirlyAnalyzer.IntegrationTests/ForecastErrorsApiControllerTests/GetRequestDatesTests.cs
+++ AirlyAnalyzer.IntegrationTests/ForecastErrorsApiControllerTests/GetRequestDatesTests.cs
@@ -2,10 +2,6 @@
 {
   using System.Net.Http;
   using System.Threading.Tasks;
-  using AirlyAnalyzer.Data;
-  using AirlyAnalyzer.IntegrationTests.Extensions;
-  using Microsoft.Extensions.DependencyInjection;
-  using Microsoft.Extensions.Logging;
   using Xunit;
 
   [Collection("ControllerIntegrationTests")]
@@ -27,40 +23,6 @@
       // Arrange
       const string requestUrl = "/api/ForecastErrorsApi/GetRequestDates";
 
-      // Act
-      var response = await _client.GetAsync(requestUrl);
-
-      // Assert
-      response.EnsureSuccessStatusCode();
-      Assert.Equal("application/json; charset=utf-8",
-          response.Content.Headers.ContentType.ToString().ToLower());
-    }
-
-    [Fact]
-    public async Task returns_success_and_correct_content_type_when_no_data_in_database()
-    {
-      // Arrange
-      const string requestUrl = "/api/ForecastErrorsApi/GetRequestDates";
-      _client = _factory.WithWebHostBuilder(builder =>
-      {
-        builder.ConfigureServices(services =>
-        {
-          services.ConfigureCommonServices();
-
-          var sp = services.BuildServiceProvider();
-
-          using (var scope = sp.CreateScope())
-          {
-            var scopedServices = scope.ServiceProvider;
-            var context = scopedServices.GetRequiredService<AirlyContext>();
-            var logger = scopedServices
-                .GetRequiredService<ILogger<GetErrorsInDayTests>>();
-
-            context.Database.EnsureCreated();
-          }
-        });
-      })
-          .CreateClient();
       // Act
       var response = await _client.GetAsync(requestUrl);
 

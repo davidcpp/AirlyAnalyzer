e917e6751b400219e3286ef97384bbed3617c438
Dawid Sroka <dawid190393@gmail.com>
2021-04-01 21:52:13 +0200

    Add new Address class to InstallationInfo

---
+++ AirlyAnalyzer/Models/AirlyAnalyzer/Address.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AirlyAnalyzer.Models
+{
+  [Owned]
+  public class Address
+  {
+    public string Country { get; set; }
+
+    public string City { get; set; }
+
+    public string Street { get; set; }
+
+    public string Number { get; set; }
+  }
+}
--- AirlyAnalyzer/Models/AirlyAnalyzer/InstallationInfo.cs
+++ AirlyAnalyzer/Models/AirlyAnalyzer/InstallationInfo.cs
@@ -1,15 +1,11 @@
-﻿namespace AirlyAnalyzer
+﻿using AirlyAnalyzer.Models;
+
+namespace AirlyAnalyzer
 {
   public class InstallationInfo
   {
     public short InstallationId { get; set; }
 
-    public string Country { get; set; }
-
-    public string City { get; set; }
-
-    public string Street { get; set; }
-
-    public string Number { get; set; }
+    public Address Address { get; set; }
   }
 }

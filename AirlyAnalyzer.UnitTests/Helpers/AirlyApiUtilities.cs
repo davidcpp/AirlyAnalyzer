6b3aeeec38ad2c0238e14332b2aa46f70d3554d4
Dawid Sroka <dawid190393@gmail.com>
2021-04-01 23:03:20 +0200

    Add AirlyApiUtilities class

---
+++ AirlyAnalyzer.UnitTests/Helpers/AirlyApiUtilities.cs
@@ -0,0 +1,38 @@
+﻿namespace AirlyAnalyzer.UnitTests.Helpers
+{
+  using AirlyAnalyzer.Models;
+  using Newtonsoft.Json;
+
+  static public class AirlyApiUtilities
+  {
+    static public string GetTestAirlyInstallationJson()
+    {
+      return @"{
+        ""id"": 6310,
+        ""location"": {
+            ""latitude"": 52.506371,
+            ""longitude"": 16.257352
+        },
+        ""locationId"": 6310,
+        ""address"": {
+            ""country"": ""Poland"",
+            ""city"": ""Pniewy"",
+            ""street"": ""Poznańska"",
+            ""number"": ""11a"",
+            ""displayAddress1"": ""Pniewy"",
+            ""displayAddress2"": ""Poznańska""
+        },
+        ""elevation"": 98.03,
+        ""airly"": true,
+        ""sponsor"": {
+            ""id"": 350,
+            ""name"": ""Pniewy"",
+            ""description"": ""Airly Sensor's sponsor"",
+            ""logo"": ""https://cdn.airly.eu/logo/Pniewy_1539543816447_2036842331.jpg"",
+            ""link"": null,
+            ""displayName"": ""Pniewy""
+        }
+        }";
+    }
+  }
+}

3b3bf4962a69a7b80f6f14241c380de48848d372
Dawid Sroka <dawid190393@gmail.com>
2021-04-02 22:13:57 +0200

    Add InstallationInfo.RequestDate property

--- AirlyAnalyzer/Data/AirlyContext.cs
+++ AirlyAnalyzer/Data/AirlyContext.cs
@@ -68,6 +68,7 @@
     {
       builder.ToTable("InstallationInfos").HasKey(x => x.InstallationId);
       builder.OwnsOne(i => i.Address);
+      builder.Property(x => x.RequestDate).HasColumnType("date");
     }
   }
 
--- AirlyAnalyzer/Models/AirlyAnalyzer/InstallationInfo.cs
+++ AirlyAnalyzer/Models/AirlyAnalyzer/InstallationInfo.cs
@@ -1,11 +1,20 @@
-﻿using AirlyAnalyzer.Models;
+﻿using System;
+using AirlyAnalyzer.Models;
 
 namespace AirlyAnalyzer
 {
   public class InstallationInfo
   {
+    private DateTime requestDate;
+
     public short InstallationId { get; set; }
 
     public Address Address { get; set; }
+
+    public DateTime RequestDate
+    {
+      get => requestDate.ToLocalTime();
+      set => requestDate = value.ToUniversalTime();
+    }
   }
 }

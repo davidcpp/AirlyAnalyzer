920b286d57746fb1e03d837046aba19f8168ec3f
Dawid Sroka <dawid190393@gmail.com>
2021-04-02 22:24:28 +0200

    Add migration for InstallationInfo.RequstDate

---
+++ AirlyAnalyzer/Migrations/20210402201914_AddRequestDatePropertyInInstallationInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirlyAnalyzer.Migrations
+{
+    public partial class AddRequestDatePropertyInInstallationInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RequestDate",
+                table: "InstallationInfos",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequestDate",
+                table: "InstallationInfos");
+        }
+    }
+}

6a09a333d7c33a3c5d86aabe6d95b53dc1eadeb1
Dawid Sroka <dawid190393@gmail.com>
2021-04-02 20:01:39 +0200

    Add BasicRepository class to repo for installations

---
+++ AirlyAnalyzer/Data/BasicRepository.cs
@@ -0,0 +1,90 @@
+﻿namespace AirlyAnalyzer.Data
+{
+  using Microsoft.EntityFrameworkCore;
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Linq.Expressions;
+  using System.Threading.Tasks;
+
+  public class BasicRepository<TEntity> : IRepository<TEntity> where TEntity : class
+  {
+    protected readonly DbSet<TEntity> _dbSet;
+    protected readonly AirlyContext _context;
+
+    public BasicRepository(AirlyContext context)
+    {
+      _context = context;
+      _dbSet = context.Set<TEntity>();
+    }
+
+    public async Task AddAsync(List<TEntity> entities)
+    {
+      for (int i = 0; i < entities.Count;)
+      {
+        if (_dbSet.Contains(entities[i]))
+        {
+          entities.RemoveAt(i);
+          continue;
+        }
+        i++;
+      }
+
+      await _dbSet.AddRangeAsync(entities);
+    }
+
+    public void Delete(Expression<Func<TEntity, bool>> wherePredicate)
+    {
+      _dbSet.RemoveRange(_dbSet.Where(wherePredicate));
+    }
+
+    public Task<List<TEntity>> Get(
+        Expression<Func<TEntity, bool>> wherePredicate = null)
+    {
+      IQueryable<TEntity> query = _dbSet;
+
+      if (wherePredicate != null)
+      {
+        query = query.Where(wherePredicate);
+      }
+
+      return query.ToListAsync();
+    }
+
+    public Task<List<T>> GetParameters<T>(
+        Expression<Func<TEntity, T>> selectPredicate,
+        Expression<Func<TEntity, bool>> wherePredicate = null,
+        Func<IQueryable<T>, IOrderedQueryable<T>> orderByMethod = null,
+        bool isDistinct = false)
+    {
+      IQueryable<TEntity> query = _dbSet;
+
+      if (wherePredicate != null)
+      {
+        query = query.Where(wherePredicate);
+      }
+
+      IQueryable<T> resultQuery;
+      if (selectPredicate != null)
+      {
+        resultQuery = query.Select(selectPredicate);
+
+        if (isDistinct)
+        {
+          resultQuery = resultQuery.Distinct();
+        }
+
+        if (orderByMethod != null)
+        {
+          resultQuery = orderByMethod(resultQuery);
+        }
+
+        return resultQuery.ToListAsync();
+      }
+      else
+      {
+        return new Task<List<T>>(() => new List<T>());
+      }
+    }
+  }
+}

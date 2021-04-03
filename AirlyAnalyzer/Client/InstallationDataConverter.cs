b7518513265350927a15df76dcf5e2796eb99f4c
Dawid Sroka <dawid190393@gmail.com>
2021-04-03 20:33:54 +0200

    Add InstallationDataConverter - convert to db type

---
+++ AirlyAnalyzer/Client/InstallationDataConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace AirlyAnalyzer.Client
+{
+  using System;
+  using AirlyAnalyzer.Models;
+
+  public static class InstallationDataConverter
+  {
+    public static InstallationInfo ConvertToInstallationInfo(
+        this Installation installation, DateTime requestDate)
+    {
+      return new InstallationInfo()
+      {
+        InstallationId = (short)installation.Id,
+        RequestDate = requestDate,
+        Address = new Address()
+        {
+          Country = installation.Address.Country ?? "",
+          City = installation.Address.City ?? "",
+          Street = installation.Address.Street ?? "",
+          Number = installation.Address.Number ?? "",
+        }
+      };
+    }
+  }
+}
+

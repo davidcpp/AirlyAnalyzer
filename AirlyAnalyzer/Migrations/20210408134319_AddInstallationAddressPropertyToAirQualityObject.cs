aee9093f3867d7e32d7d003d4ec5aad4aea7dfcf
Dawid Sroka <dawid190393@gmail.com>
2021-04-08 15:46:58 +0200

    Add AirQualityObject.InstallationAddress migration

---
+++ AirlyAnalyzer/Migrations/20210408134319_AddInstallationAddressPropertyToAirQualityObject.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirlyAnalyzer.Migrations
+{
+    public partial class AddInstallationAddressPropertyToAirQualityObject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InstallationAddress",
+                table: "Measurements",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InstallationAddress",
+                table: "Forecasts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InstallationAddress",
+                table: "ForecastErrors",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstallationAddress",
+                table: "Measurements");
+
+            migrationBuilder.DropColumn(
+                name: "InstallationAddress",
+                table: "Forecasts");
+
+            migrationBuilder.DropColumn(
+                name: "InstallationAddress",
+                table: "ForecastErrors");
+        }
+    }
+}

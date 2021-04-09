f7fe2787e978ebbc8427c9d933fbda4eed13dd46
Dawid Sroka <dawid190393@gmail.com>
2021-04-09 16:04:58 +0200

    Add display names to AirQualityForecastError

--- AirlyAnalyzer/Models/AirlyAnalyzer/AirQualityForecastError.cs
+++ AirlyAnalyzer/Models/AirlyAnalyzer/AirQualityForecastError.cs
@@ -1,13 +1,19 @@
 ﻿namespace AirlyAnalyzer.Models
 {
+  using System.ComponentModel;
+
   public class AirQualityForecastError : AirQualityObject
   {
+    [DisplayName("Caqi Error [%]")]
     public short AirlyCaqiPct { get; set; }
 
+    [DisplayName("PM2.5 Error [%]")]
     public short Pm25Pct { get; set; }
 
+    [DisplayName("PM10 Error [%]")]
     public short Pm10Pct { get; set; }
 
+    [DisplayName("Error Type")]
     public ForecastErrorType ErrorType { get; set; }
   }
 

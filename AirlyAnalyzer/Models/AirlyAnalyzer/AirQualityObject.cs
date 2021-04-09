7f29a00f669defd2b3ca79195a270f1f6113d308
Dawid Sroka <dawid190393@gmail.com>
2021-04-09 15:59:26 +0200

    Add display names to AirQualityObject

--- AirlyAnalyzer/Models/AirlyAnalyzer/AirQualityObject.cs
+++ AirlyAnalyzer/Models/AirlyAnalyzer/AirQualityObject.cs
@@ -1,6 +1,7 @@
 ﻿namespace AirlyAnalyzer.Models
 {
   using System;
+  using System.ComponentModel;
 
   public abstract class AirQualityObject
   {
@@ -8,28 +9,36 @@
     private DateTime tillDateTime;
     private DateTime requestDateTime;
 
+    [DisplayName("Installation Id")]
     public short InstallationId { get; set; }
 
+    [DisplayName("Installation Address")]
     public string InstallationAddress { get; set; }
 
+    [DisplayName("Caqi")]
     public short AirlyCaqi { get; set; }
 
+    [DisplayName("PM 2.5")]
     public short Pm25 { get; set; }
 
+    [DisplayName("PM 10")]
     public short Pm10 { get; set; }
 
+    [DisplayName("Start Time")]
     public DateTime FromDateTime
     {
       get => fromDateTime.ToLocalTime();
       set => fromDateTime = value.ToUniversalTime();
     }
 
+    [DisplayName("Time")]
     public DateTime TillDateTime
     {
       get => tillDateTime.ToLocalTime();
       set => tillDateTime = value.ToUniversalTime();
     }
 
+    [DisplayName("Request Time")]
     public DateTime RequestDateTime
     {
       get => requestDateTime.ToLocalTime();

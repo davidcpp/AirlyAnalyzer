2e908c22b415033af885692c3a7c19a27675c40c
Dawid Sroka <dawid190393@gmail.com>
2021-04-11 22:00:21 +0200

    Add migration for new AirQualityMeasurement fields

---
+++ AirlyAnalyzer/Migrations/20210411104528_AddWindParametersToAirQualityMeasurement.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirlyAnalyzer.Migrations
+{
+    public partial class AddWindParametersToAirQualityMeasurement : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<short>(
+                name: "WindBearing",
+                table: "Measurements",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<float>(
+                name: "WindSpeed",
+                table: "Measurements",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WindBearing",
+                table: "Measurements");
+
+            migrationBuilder.DropColumn(
+                name: "WindSpeed",
+                table: "Measurements");
+        }
+    }
+}

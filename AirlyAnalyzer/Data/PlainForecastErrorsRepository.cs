fa09eec5d139edc156b8ff3571439d1dbc58a639
Dawid Sroka <dawid190393@gmail.com>
2021-04-12 23:59:52 +0200

    Add Repository classes for various error classes

---
+++ AirlyAnalyzer/Data/PlainForecastErrorsRepository.cs
@@ -0,0 +1,33 @@
+﻿namespace AirlyAnalyzer.Data
+{
+  using System;
+  using System.Linq;
+  using System.Threading.Tasks;
+  using AirlyAnalyzer.Models;
+
+  public class PlainForecastErrorsRepository : ForecastErrorsRepository
+  {
+    public PlainForecastErrorsRepository(AirlyContext context) : base(context)
+    {
+    }
+
+    public override async Task<DateTime> GetLastDate(short installationId)
+    {
+      var lastDate = _dateTimeMinValue;
+
+      var selectedDates = await GetParameters(
+          wherePredicate:
+              m => m.InstallationId == installationId
+                && m.Class == ForecastErrorClass.Plain,
+          selectPredicate: m => m.TillDateTime,
+          orderByMethod: q => q.OrderByDescending(dateTime => dateTime));
+
+      if (selectedDates.Count > 0)
+      {
+        lastDate = selectedDates[0];
+      }
+
+      return lastDate;
+    }
+  }
+}

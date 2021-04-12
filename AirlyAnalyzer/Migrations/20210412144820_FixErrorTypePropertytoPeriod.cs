6d184cd8adbe0e9d29b8d6d797e956ad533188a7
Dawid Sroka <dawid190393@gmail.com>
2021-04-12 16:49:22 +0200

    Add migration for ForecastError.Period property

---
+++ AirlyAnalyzer/Migrations/20210412144820_FixErrorTypePropertytoPeriod.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirlyAnalyzer.Migrations
+{
+    public partial class FixErrorTypePropertytoPeriod : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ErrorType",
+                table: "ForecastErrors",
+                newName: "Period");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Period",
+                table: "ForecastErrors",
+                newName: "ErrorType");
+        }
+    }
+}

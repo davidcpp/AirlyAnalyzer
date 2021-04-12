735b463b7f2d3c0ddbf1e2ee6b151d91824baedd
Dawid Sroka <dawid190393@gmail.com>
2021-04-12 17:12:52 +0200

    Add migration for ForecastError.Class

---
+++ AirlyAnalyzer/Migrations/20210412150604_AddClassPropertyForForecastError.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirlyAnalyzer.Migrations
+{
+    public partial class AddClassPropertyForForecastError : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Class",
+                table: "ForecastErrors",
+                type: "varchar(20)",
+                unicode: false,
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Class",
+                table: "ForecastErrors");
+        }
+    }
+}

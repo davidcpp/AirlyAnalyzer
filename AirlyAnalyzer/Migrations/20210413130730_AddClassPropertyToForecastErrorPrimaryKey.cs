4a85a3f153cebf864f307858247bb1b66589b274
Dawid Sroka <dawid190393@gmail.com>
2021-04-13 20:08:59 +0200

    Add migration for add ForecastError.Class to PrimaryKey

---
+++ AirlyAnalyzer/Migrations/20210413130730_AddClassPropertyToForecastErrorPrimaryKey.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirlyAnalyzer.Migrations
+{
+    public partial class AddClassPropertyToForecastErrorPrimaryKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ForecastErrors",
+                table: "ForecastErrors");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ForecastErrors",
+                table: "ForecastErrors",
+                columns: new[] { "Period", "TillDateTime", "FromDateTime", "Class", "InstallationId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ForecastErrors",
+                table: "ForecastErrors");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ForecastErrors",
+                table: "ForecastErrors",
+                columns: new[] { "Period", "TillDateTime", "FromDateTime", "InstallationId" });
+        }
+    }
+}

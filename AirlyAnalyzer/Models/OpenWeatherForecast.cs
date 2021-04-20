e6fb74428fe625c169741b933d90d3255d801824
Dawid Sroka <dawid190393@gmail.com>
2021-04-20 23:46:22 +0200

    Add OpenWeatherForecast class for forecast

---
+++ AirlyAnalyzer/Models/OpenWeatherForecast.cs
@@ -0,0 +1,14 @@
+﻿namespace AirlyAnalyzer.Models
+{
+  using System.Collections.Generic;
+  using Newtonsoft.Json;
+
+  public class OpenWeatherForecast
+  {
+    [JsonProperty("timezone_offset")]
+    public int TimeZoneOffset { get; set; }
+
+    [JsonProperty("hourly")]
+    public List<OpenWeatherForecastObject> HourlyForecast { get; set; }
+  }
+}

ad9c32f1cd0050ea1cc3f535f77b5350fe08b225
Dawid Sroka <dawid190393@gmail.com>
2021-04-20 23:45:38 +0200

    Add OpenWeatherForecastObject class for forecast

---
+++ AirlyAnalyzer/Models/Meteo/OpenWeatherForecastObject.cs
@@ -0,0 +1,37 @@
+﻿namespace AirlyAnalyzer.Models
+{
+  using Newtonsoft.Json;
+
+  public class OpenWeatherForecastObject
+  {
+    [JsonProperty("dt")]
+    public long Time { get; set; }
+
+    [JsonProperty("Clouds")]
+    public byte Cloudiness { get; set; }
+
+    public byte Humidity { get; set; }
+
+    public short Pressure { get; set; }
+
+    [JsonProperty("pop")]
+    public float PropababilityOfPrecipitation { get; set; }
+
+    [JsonProperty("rain.1h")]
+    public float Rain { get; set; }
+
+    [JsonProperty("temp")]
+    public short Temperature { get; set; }
+
+    public int Visibility { get; set; }
+
+    [JsonProperty("wind_speed")]
+    public float WindSpeed { get; set; }
+
+    [JsonProperty("wind_deg")]
+    public short WindBearing { get; set; }
+
+    [JsonProperty("wind_gust")]
+    public float WindGust { get; set; }
+  }
+}

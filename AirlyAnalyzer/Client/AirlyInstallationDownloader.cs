b081cfa5a604e3670e3c1c4f0ef21a39f0b31140
Dawid Sroka <dawid190393@gmail.com>
2021-04-21 16:45:07 +0200

    Inherit AirlyApiDownloader by downloader classes

--- AirlyAnalyzer/Client/AirlyInstallationDownloader.cs
+++ AirlyAnalyzer/Client/AirlyInstallationDownloader.cs
@@ -2,49 +2,25 @@
 {
   using System;
   using System.Diagnostics;
-  using System.Net;
   using System.Threading.Tasks;
   using AirlyAnalyzer.Models;
   using Microsoft.Extensions.Configuration;
   using Newtonsoft.Json;
 
-  public class AirlyInstallationDownloader : IAirQualityDataDownloader<Installation>
+  public class AirlyInstallationDownloader : AirlyApiDownloader<Installation>
   {
-    private readonly string _airlyApiKeyHeaderName;
-    private readonly string _airlyApiKey;
-    private readonly string _contentType;
     private readonly string _installationUri;
-    private readonly string _uri;
-
-    private readonly IWebClientAdapter _webClientAdapter;
 
     public AirlyInstallationDownloader(
         IConfiguration config, IWebClientAdapter webClientAdapter)
+            : base(config, webClientAdapter)
     {
-      _airlyApiKeyHeaderName = config.GetValue<string>(
-          "AppSettings:AirlyApi:KeyHeaderName");
-
-      _airlyApiKey = config.GetValue<string>(
-          "AppSettings:AirlyApi:Key");
-
-      _contentType = config.GetValue<string>(
-          "AppSettings:AirlyApi:ContentType");
-
       _installationUri = config.GetValue<string>(
           "AppSettings:AirlyApi:InstallationUri");
-
-      _uri = config.GetValue<string>(
-          "AppSettings:AirlyApi:Uri");
-
-      _webClientAdapter = webClientAdapter;
-
-      _webClientAdapter.BaseAddress = _uri;
-      _webClientAdapter.Headers.Remove(HttpRequestHeader.Accept);
-      _webClientAdapter.Headers.Add(HttpRequestHeader.Accept, _contentType);
-      _webClientAdapter.Headers.Add(_airlyApiKeyHeaderName, _airlyApiKey);
     }
 
-    public async Task<Installation> DownloadAirQualityData(short installationId)
+    public override async Task<Installation>
+        DownloadAirQualityData(short installationId)
     {
       try
       {
@@ -69,11 +45,6 @@
 
         return new Installation();
       }
-    }
-
-    public void Dispose()
-    {
-      _webClientAdapter.Dispose();
     }
   }
 }
--- AirlyAnalyzer/Client/AirlyMeasurementsDownloader.cs
+++ AirlyAnalyzer/Client/AirlyMeasurementsDownloader.cs
@@ -2,53 +2,29 @@
 {
   using System;
   using System.Diagnostics;
-  using System.Net;
   using System.Threading.Tasks;
   using AirlyAnalyzer.Models;
   using Microsoft.Extensions.Configuration;
   using Newtonsoft.Json;
 
-  public class AirlyMeasurementsDownloader : IAirQualityDataDownloader<Measurements>
+  public class AirlyMeasurementsDownloader : AirlyApiDownloader<Measurements>
   {
-    private readonly string _airlyApiKeyHeaderName;
-    private readonly string _airlyApiKey;
-    private readonly string _contentType;
     private readonly string _measurementsUri;
     private readonly string _measurementsUriParameters;
-    private readonly string _uri;
-
-    private readonly IWebClientAdapter _webClientAdapter;
 
     public AirlyMeasurementsDownloader(
         IConfiguration config, IWebClientAdapter webClientAdapter)
+            : base(config, webClientAdapter)
     {
-      _airlyApiKeyHeaderName = config.GetValue<string>(
-          "AppSettings:AirlyApi:KeyHeaderName");
-
-      _airlyApiKey = config.GetValue<string>(
-          "AppSettings:AirlyApi:Key");
-
-      _contentType = config.GetValue<string>(
-          "AppSettings:AirlyApi:ContentType");
-
       _measurementsUri = config.GetValue<string>(
           "AppSettings:AirlyApi:MeasurementsUri");
 
       _measurementsUriParameters = config.GetValue<string>(
           "AppSettings:AirlyApi:MeasurementsUriParameters");
-
-      _uri = config.GetValue<string>(
-          "AppSettings:AirlyApi:Uri");
-
-      _webClientAdapter = webClientAdapter;
-
-      _webClientAdapter.BaseAddress = _uri;
-      _webClientAdapter.Headers.Remove(HttpRequestHeader.Accept);
-      _webClientAdapter.Headers.Add(HttpRequestHeader.Accept, _contentType);
-      _webClientAdapter.Headers.Add(_airlyApiKeyHeaderName, _airlyApiKey);
     }
 
-    public async Task<Measurements> DownloadAirQualityData(short installationId)
+    public override async Task<Measurements>
+        DownloadAirQualityData(short installationId)
     {
       try
       {
@@ -75,11 +51,6 @@
 
         return new Measurements();
       }
-    }
-
-    public void Dispose()
-    {
-      _webClientAdapter.Dispose();
     }
   }
 }

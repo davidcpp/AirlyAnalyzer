c5f1cb6a5633e163a1c1a64abd4f7dd1beb994b6
Dawid Sroka <dawid190393@gmail.com>
2021-04-21 21:46:19 +0200

    Add IAirlyApiDownloader interface

---
+++ AirlyAnalyzer/Client/IAirlyApiDownloader.cs
@@ -0,0 +1,12 @@
+﻿namespace AirlyAnalyzer.Client
+{
+  using AirlyAnalyzer.Models;
+  using System;
+  using System.Threading.Tasks;
+
+  public interface IAirlyApiDownloader : IDisposable
+  {
+    public Task<Installation> DownloadInstallationInfo(short installationId);
+    public Task<Measurements> DownloadInstallationMeasurements(short installationId);
+  }
+}

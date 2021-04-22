aaa5f8ee84a4ff49daf1ecd8a1e0fb8dfe73c45c
Dawid Sroka <dawid190393@gmail.com>
2021-04-22 23:47:10 +0200

    Add WeatherContext for AirlyCaqi predictions

---
+++ AirlyAnalyzer/Data/WeatherContext.cs
@@ -0,0 +1,36 @@
+﻿namespace AirlyAnalyzer.Data
+{
+  using AirlyAnalyzer.Models;
+  using Microsoft.EntityFrameworkCore;
+  using Microsoft.EntityFrameworkCore.Metadata;
+  using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+  public class WeatherMeasurementConfiguration
+      : IEntityTypeConfiguration<OpenWeatherForecastObject>
+  {
+    public void Configure(EntityTypeBuilder<OpenWeatherForecastObject> builder)
+    {
+      builder.ToTable("WeatherMeasurements").HasKey(x => x.Time);
+      builder.OwnsOne(i => i.Rain);
+    }
+  }
+
+  public class WeatherContext : DbContext
+  {
+    public WeatherContext(DbContextOptions<WeatherContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<OpenWeatherForecastObject> WeatherMeasurements { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.ApplyConfiguration(new WeatherMeasurementConfiguration());
+
+      modelBuilder.HasAnnotation(
+          "SqlServer:ValueGenerationStrategy",
+          SqlServerValueGenerationStrategy.None);
+    }
+  }
+}

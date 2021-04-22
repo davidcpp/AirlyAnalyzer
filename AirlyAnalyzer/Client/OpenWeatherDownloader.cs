41d657e34abe2defd7ac514e70bd957f05160431
Dawid Sroka <dawid190393@gmail.com>
2021-04-22 12:28:21 +0200

    Move downloading to OpenWeatherDownloader class

--- AirlyAnalyzer/Client/OpenWeatherDownloader.cs
+++ AirlyAnalyzer/Client/OpenWeatherDownloader.cs
@@ -1,10 +1,16 @@
 ﻿namespace AirlyAnalyzer.Client
 {
   using System;
+  using System.Diagnostics;
+  using System.Threading.Tasks;
+  using AirlyAnalyzer.Models;
   using Microsoft.Extensions.Configuration;
+  using Newtonsoft.Json;
 
-  public abstract class OpenWeatherDownloader : IDisposable
+  public class OpenWeatherDownloader : IDisposable
   {
+    private readonly IConfiguration _config;
+
     protected readonly string _apiKeyParameter;
     protected readonly string _unitsParameter;
 
@@ -13,9 +19,11 @@
 
     protected IWebClientAdapter _webClientAdapter;
 
-    protected OpenWeatherDownloader(
+    public OpenWeatherDownloader(
         IConfiguration config, IWebClientAdapter webClientAdapter)
     {
+      _config = config;
+
       string uri = config.GetValue<string>(
           "AppSettings:OpenWeatherApi:Uri");
 
@@ -33,6 +41,53 @@
 
       _webClientAdapter = webClientAdapter;
       _webClientAdapter.BaseAddress = uri;
+    }
+
+    public async Task<OpenWeatherForecast> DownloadHourlyWeatherForecast(
+        float latitude, float longtitude)
+    {
+      try
+      {
+        const string _exclude = "minutely,daily,current,alerts";
+
+        string forecastUri = _config.GetValue<string>(
+            "AppSettings:OpenWeatherApi:ForecastUri");
+
+        string latitudeParameter = _config.GetValue<string>(
+            "AppSettings:OpenWeatherApi:LatitudeParameter");
+
+        string longtitudeParameter = _config.GetValue<string>(
+            "AppSettings:OpenWeatherApi:LongtitudeParameter");
+
+        string excludeParameter = _config.GetValue<string>(
+            "AppSettings:OpenWeatherApi:ExcludeParameter");
+
+        string response = await _webClientAdapter.DownloadStringTaskAsync(
+            forecastUri
+            + _apiKeyParameter + _apiKey + "&"
+            + latitudeParameter + latitude + "&"
+            + longtitudeParameter + longtitude + "&"
+            + _unitsParameter + _units + "&"
+            + excludeParameter + _exclude);
+
+        return JsonConvert.DeserializeObject<OpenWeatherForecast>(
+            response,
+            new JsonSerializerSettings()
+            {
+              NullValueHandling = NullValueHandling.Ignore
+            })
+          ?? new OpenWeatherForecast();
+      }
+      catch (Exception e)
+      {
+        string stackTrace = e.StackTrace;
+        Debug.WriteLine(stackTrace);
+        Debug.WriteLine(e.Message);
+        Debug.WriteLine(e.Source);
+        Debug.WriteLine("\n");
+
+        return new OpenWeatherForecast();
+      }
     }
 
     public void Dispose()

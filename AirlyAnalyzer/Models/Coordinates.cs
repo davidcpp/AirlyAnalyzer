ba9b6403ef22a234d8b0c9eadf493b0d5c4f6d32
Dawid Sroka <dawid190393@gmail.com>
2021-04-30 18:04:18 +0200

    Fix Coordinates property types to float

--- AirlyAnalyzer/Models/Coordinates.cs
+++ AirlyAnalyzer/Models/Coordinates.cs
@@ -5,8 +5,8 @@
   [Owned]
   public class Coordinates
   {
-    public double Latitude { get; set; }
+    public float Latitude { get; set; }
 
-    public double Longitude { get; set; }
+    public float Longitude { get; set; }
   }
 }

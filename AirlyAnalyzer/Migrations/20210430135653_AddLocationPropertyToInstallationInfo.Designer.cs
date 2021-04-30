c97361d6dda76289d21d99eebc230a91afe9c8db
Dawid Sroka <dawid190393@gmail.com>
2021-04-30 17:30:20 +0200

    Add migration for InstallationInfo.Location

--- AirlyAnalyzer/Migrations/20210430135653_AddLocationPropertyToInstallationInfo.Designer.cs
+++ AirlyAnalyzer/Migrations/20210430135653_AddLocationPropertyToInstallationInfo.Designer.cs
@@ -4,14 +4,16 @@ using AirlyAnalyzer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AirlyAnalyzer.Migrations
 {
     [DbContext(typeof(AirlyContext))]
-    partial class AirlyContextModelSnapshot : ModelSnapshot
+    [Migration("20210430135653_AddLocationPropertyToInstallationInfo")]
+    partial class AddLocationPropertyToInstallationInfo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AirlyAnalyzer/Migrations/20210430135653_AddLocationPropertyToInstallationInfo.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirlyAnalyzer.Migrations
+{
+    public partial class AddLocationPropertyToInstallationInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Location_Latitude",
+                table: "InstallationInfos",
+                type: "float",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Location_Longitude",
+                table: "InstallationInfos",
+                type: "float",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Location_Latitude",
+                table: "InstallationInfos");
+
+            migrationBuilder.DropColumn(
+                name: "Location_Longitude",
+                table: "InstallationInfos");
+        }
+    }
+}

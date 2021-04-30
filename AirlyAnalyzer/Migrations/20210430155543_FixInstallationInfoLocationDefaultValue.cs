7cd4d50ba8f5eb447b497b9f633084488299b5ad
Dawid Sroka <dawid190393@gmail.com>
2021-04-30 18:05:08 +0200

    Add migration for fix Coordinates property types

---
+++ AirlyAnalyzer/Migrations/20210430155543_FixInstallationInfoLocationDefaultValue.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirlyAnalyzer.Migrations
+{
+    public partial class FixInstallationInfoLocationDefaultValue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Location_Longitude",
+                table: "InstallationInfos",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Location_Latitude",
+                table: "InstallationInfos",
+                type: "real",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Location_Longitude",
+                table: "InstallationInfos",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Location_Latitude",
+                table: "InstallationInfos",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(float),
+                oldType: "real",
+                oldNullable: true);
+        }
+    }
+}

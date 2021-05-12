ff1adb04b32c2912751383ed80bb81f125cce930
Dawid Sroka <dawid190393@gmail.com>
2021-05-12 18:28:18 +0200

    Remove redundant using directives in ConsumeModel

--- AirlyAnalyzerML.Model/ConsumeModel.cs
+++ AirlyAnalyzerML.Model/ConsumeModel.cs
@@ -2,11 +2,7 @@
 
 using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Microsoft.ML;
-using AirlyAnalyzerML.Model;
 
 namespace AirlyAnalyzerML.Model
 {

a1932dc6f19f9abe33e70123d72900edc6683206
Dawid Sroka <dawid190393@gmail.com>
2021-05-14 22:52:08 +0200

    Add OpenWeatherApiUtilities class for Unit Tests

---
+++ AirlyAnalyzer.UnitTests/Helpers/OpenWeatherApiUtilities.cs
@@ -0,0 +1,33 @@
+﻿namespace AirlyAnalyzer.UnitTests.Helpers
+{
+  using System;
+  using System.IO;
+  using System.Reflection;
+
+  static public class OpenWeatherApiUtilities
+  {
+    static public string GetTestOpenWeatherApiForecastJson()
+    {
+      string currentAssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+      string a2 = Assembly.GetCallingAssembly().GetName().Name;
+      //var a3 = Assembly.GetEntryAssembly().GetName().Name;
+
+      string solutionFolder = Environment.CurrentDirectory;
+
+      solutionFolder = solutionFolder.Substring(
+          0,
+          solutionFolder.IndexOf(currentAssemblyName)
+          + currentAssemblyName.Length);
+
+      string forecastJsonFilepath = Path.Combine(
+          solutionFolder,
+          @"Helpers\Data",
+          "OpenWeatherForecastExample.json");
+
+      using (var file = new StreamReader(forecastJsonFilepath))
+      {
+        return file.ReadToEnd();
+      }
+    }
+  }
+}

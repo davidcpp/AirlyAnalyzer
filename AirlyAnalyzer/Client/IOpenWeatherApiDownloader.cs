85670f444fd05ebb9f376e4e359603accb1be2c7
Dawid Sroka <dawid190393@gmail.com>
2021-05-16 22:33:24 +0200

    Add IOpenWeatherApiDownloader interface

---
+++ AirlyAnalyzer/Client/IOpenWeatherApiDownloader.cs
@@ -0,0 +1,11 @@
+﻿namespace AirlyAnalyzer.Client
+{
+  using System;
+  using System.Threading.Tasks;
+  using AirlyAnalyzer.Models;
+
+  public interface IOpenWeatherApiDownloader : IDisposable
+  {
+    public Task<OpenWeatherForecast> DownloadHourlyWeatherForecast(float latitude, float longtitude);
+  }
+}

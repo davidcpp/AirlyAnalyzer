8683f27817ee6121eec3cd01b4cef8d853f695f9
Dawid Sroka <dawid190393@gmail.com>
2021-05-17 19:54:21 +0200

    Add Unit Test for AirQualityForecastController
    Add first Unit Test for not yet created DownloadHourlyWeatherForecasts()
    method of not yet created AirQualityForecastController class. Therefore
    the Unit Test project doesn't compile.

---
+++ AirlyAnalyzer.UnitTests/AirQualityForecastControllerTests/DownloadHourlyWeatherForecastsTest.cs
@@ -0,0 +1,53 @@
+﻿namespace AirlyAnalyzer.UnitTests.AirQualityForecastControllerTests
+{
+  using System.Threading.Tasks;
+  using AirlyAnalyzer.Client;
+  using AirlyAnalyzer.Models;
+  using AirlyAnalyzer.Services;
+  using Microsoft.Extensions.Configuration;
+  using Microsoft.Extensions.DependencyInjection;
+  using Moq;
+  using Xunit;
+
+  public class DownloadHourlyWeatherForecastsTest
+  {
+    private readonly Mock<IOpenWeatherApiDownloader> _downloaderMock;
+
+    public DownloadHourlyWeatherForecastsTest()
+    {
+      _downloaderMock = new Mock<IOpenWeatherApiDownloader>();
+    }
+
+    [Fact]
+    public async Task does_not_download_when_no_installations()
+    {
+      // Arrange
+      var downloadedData = new OpenWeatherForecast();
+
+      _downloaderMock.Setup(
+          x => x.DownloadHourlyWeatherForecast(
+              It.IsAny<float>(), It.IsAny<float>()))
+                     .ReturnsAsync(downloadedData);
+
+      var services = new ServiceCollection();
+      services.AddSingleton(_downloaderMock.Object);
+      var serviceProvider = services.BuildServiceProvider();
+
+      var config = new ConfigurationBuilder()
+          .Build();
+
+      var airQualityForecastController = new AirQualityForecastController(
+          serviceProvider, config);
+
+      // Act
+      var weatherForecastsList
+          = await airQualityForecastController.DownloadHourlyWeatherForecasts();
+
+      // Assert
+      _downloaderMock.Verify(
+          x => x.DownloadHourlyWeatherForecast(
+              It.IsAny<float>(), It.IsAny<float>()),
+          Times.Never());
+    }
+  }
+}

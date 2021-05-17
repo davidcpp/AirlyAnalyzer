289acfa425e21cd75a134293eaeaa1d87c5645ff
Dawid Sroka <dawid190393@gmail.com>
2021-05-17 23:24:58 +0200

    DownloadHourlyWeatherForecasts() TDD: red

--- AirlyAnalyzer.UnitTests/AirQualityForecastControllerTests/DownloadHourlyWeatherForecastsTest.cs
+++ AirlyAnalyzer.UnitTests/AirQualityForecastControllerTests/DownloadHourlyWeatherForecastsTest.cs
@@ -56,5 +56,45 @@
               It.IsAny<float>(), It.IsAny<float>()),
           Times.Never());
     }
+
+    [Fact]
+    public async Task downloads_for_all_installations()
+    {
+      // Arrange
+      var downloadedData = new OpenWeatherForecast();
+
+      _downloaderMock.Setup(
+          x => x.DownloadHourlyWeatherForecast(
+              It.IsAny<float>(), It.IsAny<float>()))
+                     .ReturnsAsync(downloadedData);
+
+      var services = new ServiceCollection();
+      services.AddSingleton(_downloaderMock.Object);
+      var serviceProvider = services.BuildServiceProvider();
+
+      var configInstallationIds = new List<KeyValuePair<string, string>>
+      {
+        new KeyValuePair<string, string>("AppSettings:AirlyApi:InstallationIds:0", "2"),
+        new KeyValuePair<string, string>("AppSettings:AirlyApi:InstallationIds:1", "4"),
+        new KeyValuePair<string, string>("AppSettings:AirlyApi:InstallationIds:2", "6"),
+      };
+
+      var config = new ConfigurationBuilder()
+          .AddInMemoryCollection(configInstallationIds)
+          .Build();
+
+      var airQualityForecastController = new AirQualityForecastController(
+          serviceProvider, config);
+
+      // Act
+      var weatherForecastsList
+          = await airQualityForecastController.DownloadHourlyWeatherForecasts();
+
+      // Assert
+      _downloaderMock.Verify(
+          x => x.DownloadHourlyWeatherForecast(
+              It.IsAny<float>(), It.IsAny<float>()),
+          Times.Exactly(configInstallationIds.Count));
+    }
   }
 }

12b865795a91bb638f889f397a82fc5dd009d6a3
Dawid Sroka <dawid190393@gmail.com>
2021-05-18 22:14:49 +0200

    DownloadHourlyWeatherForecasts() TDD: red

--- AirlyAnalyzer.UnitTests/AirQualityForecastControllerTests/DownloadHourlyWeatherForecastsTest.cs
+++ AirlyAnalyzer.UnitTests/AirQualityForecastControllerTests/DownloadHourlyWeatherForecastsTest.cs
@@ -72,6 +72,61 @@
     }
 
     [Fact]
+    public async Task does_not_download_when_air_quality_forecasts_is_up_to_date()
+    {
+      // Arrange
+      const short numberOfDays = 3;
+      const short updateHoursPeriod = 12;
+      const short numberOfForecastsInDay = 24;
+
+      var startDate = DateTime.UtcNow.AddHours(-(4 * updateHoursPeriod) + 1);
+      var installationIds = new List<short> { 2, 4, 6 };
+
+      var services = new ServiceCollection();
+      services.AddSingleton(_downloaderMock.Object);
+      var serviceProvider = services.BuildServiceProvider();
+
+      _context.AddAllForecastsToDatabase(
+          installationIds,
+          startDate,
+          numberOfDays,
+          numberOfForecastsInDay,
+          AirQualityForecastSource.App);
+
+      var configInstallationIds = new List<KeyValuePair<string, string>>
+      {
+        new KeyValuePair<string, string>(
+            "AppSettings:AirQualityForecast:UpdateHoursPeriod",
+            updateHoursPeriod.ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:0", installationIds[0].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:1", installationIds[1].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:2", installationIds[2].ToString()),
+      };
+
+      var config = new ConfigurationBuilder()
+          .AddInMemoryCollection(configInstallationIds)
+          .Build();
+
+      var airQualityForecastController = new AirQualityForecastController(
+          serviceProvider, config);
+
+      // Act
+      var weatherForecastsList
+          = await airQualityForecastController.DownloadHourlyWeatherForecasts();
+
+      // Assert
+      _downloaderMock.Verify(
+          x => x.DownloadHourlyWeatherForecast(
+              It.IsAny<float>(), It.IsAny<float>()),
+          Times.Never());
+
+      Assert.Empty(weatherForecastsList);
+    }
+
+    [Fact]
     public async Task downloads_for_all_installations()
     {
       // Arrange

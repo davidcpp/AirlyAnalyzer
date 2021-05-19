f6c787378f6fa9cac9e40ece2d2135dff9338526
Dawid Sroka <dawid190393@gmail.com>
2021-05-19 20:48:52 +0200

    Add OpenWeatherForecastTest class Unit Test

--- AirlyAnalyzer.UnitTests/ModelsTests/OpenWeatherApi/OpenWeatherForecastTest.cs
+++ AirlyAnalyzer.UnitTests/ModelsTests/OpenWeatherApi/OpenWeatherForecastTest.cs
@@ -14,5 +14,15 @@
       // Assert
       Assert.NotNull(openWeatherForecast.HourlyForecast);
     }
+
+    [Fact]
+    public void time_zone_offset_property_equals_zero_for_new_object()
+    {
+      // Act
+      var openWeatherForecast = new OpenWeatherForecast();
+
+      // Assert
+      Assert.Equal(0, openWeatherForecast.TimeZoneOffset);
+    }
   }
 }

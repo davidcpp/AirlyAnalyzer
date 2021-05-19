c0180d9737bde0c8fb2802b7ad5376a2069473fb
Dawid Sroka <dawid190393@gmail.com>
2021-05-19 20:42:39 +0200

    OpenWeatherForecast class TDD: red

---
+++ AirlyAnalyzer.UnitTests/ModelsTests/OpenWeatherApi/OpenWeatherForecastTest.cs
@@ -0,0 +1,18 @@
+﻿namespace AirlyAnalyzer.UnitTests.ModelsTests.OpenWeatherApi
+{
+  using AirlyAnalyzer.Models;
+  using Xunit;
+
+  public class OpenWeatherForecastTest
+  {
+    [Fact]
+    public void returns_not_null_hourly_forecast()
+    {
+      // Act
+      var openWeatherForecast = new OpenWeatherForecast();
+
+      // Assert
+      Assert.NotNull(openWeatherForecast.HourlyForecast);
+    }
+  }
+}

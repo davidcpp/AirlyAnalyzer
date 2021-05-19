4e3214d644e3a8663b2d32d4c69348563aed42dc
Dawid Sroka <dawid190393@gmail.com>
2021-05-19 23:54:10 +0200

    Add DownloadHourlyWeatherForecasts() Unit Tests

--- AirlyAnalyzer.UnitTests/AirQualityForecastControllerTests/DownloadHourlyWeatherForecastsTest.cs
+++ AirlyAnalyzer.UnitTests/AirQualityForecastControllerTests/DownloadHourlyWeatherForecastsTest.cs
@@ -200,6 +200,258 @@
     }
 
     [Fact]
+    public async Task downloads_for_all_installations_when_all_air_quality_forecasts_is_out_of_date()
+    {
+      // Arrange
+      const short numberOfDays = 3;
+      const short updateHoursPeriod = 12;
+      const short numberOfForecastsInDay = 24;
+
+      var startDate = DateTime.UtcNow.AddHours(-(8 * updateHoursPeriod) + 1);
+      var installationIds = new List<short> { 2, 4, 6 };
+
+      var services = new ServiceCollection();
+      services.AddSingleton(_downloaderMock.Object);
+      var serviceProvider = services.BuildServiceProvider();
+
+      _context.AddAllForecastsToDatabase(
+          installationIds,
+          startDate,
+          numberOfDays,
+          numberOfForecastsInDay,
+          AirQualityForecastSource.App);
+
+      var configInstallationIds = new List<KeyValuePair<string, string>>
+      {
+        new KeyValuePair<string, string>(
+            "AppSettings:AirQualityForecast:UpdateHoursPeriod",
+            updateHoursPeriod.ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:0", installationIds[0].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:1", installationIds[1].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:2", installationIds[2].ToString()),
+      };
+
+      var config = new ConfigurationBuilder()
+          .AddInMemoryCollection(configInstallationIds)
+          .Build();
+
+      var airQualityForecastController = new AirQualityForecastController(
+          serviceProvider, config, unitOfWork: _unitOfWork);
+
+      // Act
+      var weatherForecastsList
+          = await airQualityForecastController.DownloadHourlyWeatherForecasts();
+
+      // Assert
+      _downloaderMock.Verify(
+          x => x.DownloadHourlyWeatherForecast(
+              It.IsAny<float>(), It.IsAny<float>()),
+          Times.Exactly(installationIds.Count));
+    }
+
+    [Fact]
+    public async Task returns_downloaded_for_all_installations_when_all_air_quality_forecasts_is_out_of_date()
+    {
+      // Arrange
+      const short numberOfDays = 3;
+      const short updateHoursPeriod = 12;
+      const short numberOfForecastsInDay = 24;
+
+      var startDate = DateTime.UtcNow.AddHours(-(8 * updateHoursPeriod) + 1);
+      var installationIds = new List<short> { 2, 4, 6 };
+
+      var downloadedData = new OpenWeatherForecast();
+      downloadedData.HourlyForecast.Add(new OpenWeatherForecastObject());
+
+      _downloaderMock.Setup(
+          x => x.DownloadHourlyWeatherForecast(
+              It.IsAny<float>(), It.IsAny<float>()))
+                     .ReturnsAsync(downloadedData);
+
+      var services = new ServiceCollection();
+      services.AddSingleton(_downloaderMock.Object);
+      var serviceProvider = services.BuildServiceProvider();
+
+      _context.AddAllForecastsToDatabase(
+          installationIds,
+          startDate,
+          numberOfDays,
+          numberOfForecastsInDay,
+          AirQualityForecastSource.App);
+
+      var configInstallationIds = new List<KeyValuePair<string, string>>
+      {
+        new KeyValuePair<string, string>(
+            "AppSettings:AirQualityForecast:UpdateHoursPeriod",
+            updateHoursPeriod.ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:0", installationIds[0].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:1", installationIds[1].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:2", installationIds[2].ToString()),
+      };
+
+      var config = new ConfigurationBuilder()
+          .AddInMemoryCollection(configInstallationIds)
+          .Build();
+
+      var airQualityForecastController = new AirQualityForecastController(
+          serviceProvider, config, unitOfWork: _unitOfWork);
+
+      // Act
+      var weatherForecastsList
+          = await airQualityForecastController.DownloadHourlyWeatherForecasts();
+
+      // Assert
+      Assert.Equal(installationIds.Count, weatherForecastsList.Count);
+      Assert.Equal(
+          downloadedData.HourlyForecast.Count,
+          weatherForecastsList[0].HourlyForecast.Count);
+      Assert.Equal(
+          downloadedData.HourlyForecast.Count,
+          weatherForecastsList[1].HourlyForecast.Count);
+      Assert.Equal(
+          downloadedData.HourlyForecast.Count,
+          weatherForecastsList[2].HourlyForecast.Count);
+    }
+
+    [Fact]
+    public async Task downloads_for_some_installations_when_some_air_quality_forecasts_is_out_of_date()
+    {
+      // Arrange
+      const short numberOfDays = 3;
+      const short updateHoursPeriod = 12;
+      const short numberOfForecastsInDay = 24;
+
+      var startDate = DateTime.UtcNow.AddHours(-(8 * updateHoursPeriod) + 1);
+      var installationIds = new List<short> { 2, 4, 6 };
+
+      var services = new ServiceCollection();
+      services.AddSingleton(_downloaderMock.Object);
+      var serviceProvider = services.BuildServiceProvider();
+
+      _context.AddAllForecastsToDatabase(
+          installationIds,
+          startDate,
+          numberOfDays,
+          numberOfForecastsInDay,
+          AirQualityForecastSource.App);
+
+      _context.AddForecastsToDatabase(
+          installationIds[1],
+          startDate.AddDays(numberOfDays),
+          1,
+          updateHoursPeriod,
+          AirQualityForecastSource.App);
+
+      var configInstallationIds = new List<KeyValuePair<string, string>>
+      {
+        new KeyValuePair<string, string>(
+            "AppSettings:AirQualityForecast:UpdateHoursPeriod",
+            updateHoursPeriod.ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:0", installationIds[0].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:1", installationIds[1].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:2", installationIds[2].ToString()),
+      };
+
+      var config = new ConfigurationBuilder()
+          .AddInMemoryCollection(configInstallationIds)
+          .Build();
+
+      var airQualityForecastController = new AirQualityForecastController(
+          serviceProvider, config, unitOfWork: _unitOfWork);
+
+      // Act
+      var weatherForecastsList
+          = await airQualityForecastController.DownloadHourlyWeatherForecasts();
+
+      // Assert
+      _downloaderMock.Verify(
+          x => x.DownloadHourlyWeatherForecast(
+              It.IsAny<float>(), It.IsAny<float>()),
+          Times.Exactly(installationIds.Count - 1));
+    }
+
+    [Fact]
+    public async Task returns_downloaded_for_some_installations_when_some_air_quality_forecasts_is_out_of_date()
+    {
+      // Arrange
+      const short numberOfDays = 3;
+      const short updateHoursPeriod = 12;
+      const short numberOfForecastsInDay = 24;
+
+      var startDate = DateTime.UtcNow.AddHours(-(8 * updateHoursPeriod) + 1);
+      var installationIds = new List<short> { 2, 4, 6 };
+
+      var downloadedData = new OpenWeatherForecast();
+      downloadedData.HourlyForecast.Add(new OpenWeatherForecastObject());
+
+      _downloaderMock.Setup(
+          x => x.DownloadHourlyWeatherForecast(
+              It.IsAny<float>(), It.IsAny<float>()))
+                     .ReturnsAsync(downloadedData);
+
+      var services = new ServiceCollection();
+      services.AddSingleton(_downloaderMock.Object);
+      var serviceProvider = services.BuildServiceProvider();
+
+      _context.AddAllForecastsToDatabase(
+          installationIds,
+          startDate,
+          numberOfDays,
+          numberOfForecastsInDay,
+          AirQualityForecastSource.App);
+
+      _context.AddForecastsToDatabase(
+          installationIds[1],
+          startDate.AddDays(numberOfDays),
+          1,
+          updateHoursPeriod,
+          AirQualityForecastSource.App);
+
+      var configInstallationIds = new List<KeyValuePair<string, string>>
+      {
+        new KeyValuePair<string, string>(
+            "AppSettings:AirQualityForecast:UpdateHoursPeriod",
+            updateHoursPeriod.ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:0", installationIds[0].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:1", installationIds[1].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:2", installationIds[2].ToString()),
+      };
+
+      var config = new ConfigurationBuilder()
+          .AddInMemoryCollection(configInstallationIds)
+          .Build();
+
+      var airQualityForecastController = new AirQualityForecastController(
+          serviceProvider, config, unitOfWork: _unitOfWork);
+
+      // Act
+      var weatherForecastsList
+          = await airQualityForecastController.DownloadHourlyWeatherForecasts();
+
+      // Assert
+      Assert.Equal(installationIds.Count, weatherForecastsList.Count);
+      Assert.Equal(
+          downloadedData.HourlyForecast.Count,
+          weatherForecastsList[0].HourlyForecast.Count);
+      Assert.NotNull(weatherForecastsList[1]);
+      Assert.Equal(
+          downloadedData.HourlyForecast.Count,
+          weatherForecastsList[2].HourlyForecast.Count);
+    }
+
+    [Fact]
     public async Task downloads_for_all_installations_when_no_air_quality_forecasts_in_database()
     {
       // Arrange

299d9586adb378f09f7a25fdbfecd9efb862646c
Dawid Sroka <dawid190393@gmail.com>
2021-05-20 23:39:01 +0200

    DownloadHourlyWeatherForecasts() TDD: red

--- AirlyAnalyzer.UnitTests/AirQualityForecastControllerTests/DownloadHourlyWeatherForecastsTest.cs
+++ AirlyAnalyzer.UnitTests/AirQualityForecastControllerTests/DownloadHourlyWeatherForecastsTest.cs
@@ -671,5 +671,117 @@
           downloadedData.HourlyForecast.Count,
           weatherForecastsList[2].HourlyForecast.Count);
     }
+
+    [Fact]
+    public async Task downloads_with_correct_parameters()
+    {
+      // Arrange
+      var downloadedData = new List<OpenWeatherForecast>
+      {
+        new OpenWeatherForecast { TimeZoneOffset = 3600 },
+        new OpenWeatherForecast { TimeZoneOffset = 7200 },
+        new OpenWeatherForecast { TimeZoneOffset = 10800 },
+      };
+
+      var downloaderMock = new Mock<IOpenWeatherApiDownloader>(MockBehavior.Strict);
+      var mockSequence = new MockSequence();
+
+      for (int i = 0; i < installationIds.Count; i++)
+      {
+        downloaderMock.InSequence(mockSequence)
+            .Setup(x => x.DownloadHourlyWeatherForecast(
+                _installationInfos[i].Location.Latitude,
+                _installationInfos[i].Location.Longitude))
+                      .ReturnsAsync(downloadedData[i]);
+      }
+
+      var services = new ServiceCollection();
+      services.AddSingleton(downloaderMock.Object);
+      var serviceProvider = services.BuildServiceProvider();
+
+      var configInstallationIds = new List<KeyValuePair<string, string>>
+      {
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:0", installationIds[0].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:1", installationIds[1].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:2", installationIds[2].ToString()),
+      };
+
+      var config = new ConfigurationBuilder()
+          .AddInMemoryCollection(configInstallationIds)
+          .Build();
+
+      var airQualityForecastController = new AirQualityForecastController(
+          serviceProvider, config, unitOfWork: _unitOfWork);
+
+      // Act
+      var weatherForecastsList
+          = await airQualityForecastController.DownloadHourlyWeatherForecasts();
+
+      // Assert
+      downloaderMock.Verify(
+          x => x.DownloadHourlyWeatherForecast(
+              It.IsAny<float>(), It.IsAny<float>()),
+          Times.Exactly(installationIds.Count));
+    }
+
+    [Fact]
+    public async Task returns_correct_weather_forecast_objects()
+    {
+      // Arrange
+      var downloadedData = new List<OpenWeatherForecast>
+      {
+        new OpenWeatherForecast { TimeZoneOffset = 3600 },
+        new OpenWeatherForecast { TimeZoneOffset = 7200 },
+        new OpenWeatherForecast { TimeZoneOffset = 10800 },
+      };
+
+      var downloaderMock = new Mock<IOpenWeatherApiDownloader>(MockBehavior.Strict);
+      var mockSequence = new MockSequence();
+
+      for (int i = 0; i < installationIds.Count; i++)
+      {
+        downloaderMock.InSequence(mockSequence)
+            .Setup(x => x.DownloadHourlyWeatherForecast(
+                It.IsAny<float>(), It.IsAny<float>()))
+                      .ReturnsAsync(downloadedData[i]);
+      }
+
+      var services = new ServiceCollection();
+      services.AddSingleton(downloaderMock.Object);
+      var serviceProvider = services.BuildServiceProvider();
+
+      var configInstallationIds = new List<KeyValuePair<string, string>>
+      {
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:0", installationIds[0].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:1", installationIds[1].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:2", installationIds[2].ToString()),
+      };
+
+      var config = new ConfigurationBuilder()
+          .AddInMemoryCollection(configInstallationIds)
+          .Build();
+
+      var airQualityForecastController = new AirQualityForecastController(
+          serviceProvider, config, unitOfWork: _unitOfWork);
+
+      // Act
+      var weatherForecastsList
+          = await airQualityForecastController.DownloadHourlyWeatherForecasts();
+
+      // Assert
+      Assert.Equal(installationIds.Count, weatherForecastsList.Count);
+      Assert.Equal(
+          downloadedData[0].TimeZoneOffset, weatherForecastsList[0].TimeZoneOffset);
+      Assert.Equal(
+          downloadedData[1].TimeZoneOffset, weatherForecastsList[1].TimeZoneOffset);
+      Assert.Equal(
+          downloadedData[2].TimeZoneOffset, weatherForecastsList[2].TimeZoneOffset);
+    }
   }
 }

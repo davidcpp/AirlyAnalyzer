7991e78fc23f33c89b5480686eca88ade3a3ca58
Dawid Sroka <dawid190393@gmail.com>
2021-05-23 23:50:07 +0200

    Add WeatherObjectsConverter class

---
+++ AirlyAnalyzer/Client/WeatherObjectsConverter.cs
@@ -0,0 +1,34 @@
+﻿namespace AirlyAnalyzer.Client
+{
+  using System;
+  using AirlyAnalyzer.Models;
+  using AirlyAnalyzer.Models.Weather;
+
+  public static class WeatherObjectsConverter
+  {
+    public static WeatherMeasurement ConvertToWeatherMeasurement(
+        this OpenWeatherForecastObject weatherForecastItem, int installationId)
+    {
+      var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)
+          .AddSeconds(weatherForecastItem.Time);
+
+      return new WeatherMeasurement()
+      {
+        Year = (short)dateTime.Year,
+        Month = (byte)dateTime.Month,
+        Day = (byte)dateTime.Day,
+        Hour = (byte)dateTime.Hour,
+        InstallationId = installationId,
+        CloudCover = weatherForecastItem.Cloudiness,
+        Humidity = weatherForecastItem.Humidity,
+        Pressure = weatherForecastItem.Pressure,
+        Temperature = weatherForecastItem.Temperature,
+        Visibility = weatherForecastItem.Visibility,
+        WindSpeed = weatherForecastItem.WindSpeed,
+        WindDirection = weatherForecastItem.WindDirection,
+        WindGust = weatherForecastItem.WindGust,
+      };
+    }
+  }
+}
+

0395f6f38c52a3562e3f606e3c56fe9764507fc8
Dawid Sroka <dawid190393@gmail.com>
2021-05-24 15:48:48 +0200

    Add Unit Tests when no installationInfo TDD:red

--- AirlyAnalyzer.UnitTests/AirQualityForecastControllerTests/DownloadHourlyWeatherForecastsTest.cs
+++ AirlyAnalyzer.UnitTests/AirQualityForecastControllerTests/DownloadHourlyWeatherForecastsTest.cs
@@ -207,6 +207,94 @@
     }
 
     [Fact]
+    public async Task does_not_download_when_no_installation_infos()
+    {
+      // Arrange
+      const short updateHoursPeriod = 12;
+
+      var services = new ServiceCollection();
+      services.AddSingleton(_downloaderMock.Object);
+      var serviceProvider = services.BuildServiceProvider();
+
+      _context.InstallationInfos.RemoveRange(_context.InstallationInfos);
+      _context.SaveChanges();
+
+      var configInstallationIds = new List<KeyValuePair<string, string>>
+      {
+        new KeyValuePair<string, string>(
+            "AppSettings:AirQualityForecast:UpdateHoursPeriod",
+            updateHoursPeriod.ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:0", _installationIds[0].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:1", _installationIds[1].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:2", _installationIds[2].ToString()),
+      };
+
+      var config = new ConfigurationBuilder()
+          .AddInMemoryCollection(configInstallationIds)
+          .Build();
+
+      var airQualityForecastController = new AirQualityForecastController(
+          serviceProvider, config, unitOfWork: _unitOfWork);
+
+      // Act
+      var weatherForecastsList
+          = await airQualityForecastController.DownloadHourlyWeatherForecasts();
+
+      // Assert
+      _downloaderMock.Verify(
+          x => x.DownloadHourlyWeatherForecast(
+              It.IsAny<float>(), It.IsAny<float>()),
+          Times.Never());
+    }
+
+    [Fact]
+    public async Task returns_new_empty_forecasts_for_all_installations_when_no_installation_infos()
+    {
+      // Arrange
+      const short updateHoursPeriod = 12;
+
+      var services = new ServiceCollection();
+      services.AddSingleton(_downloaderMock.Object);
+      var serviceProvider = services.BuildServiceProvider();
+
+      _context.InstallationInfos.RemoveRange(_context.InstallationInfos);
+      _context.SaveChanges();
+
+      var configInstallationIds = new List<KeyValuePair<string, string>>
+      {
+        new KeyValuePair<string, string>(
+            "AppSettings:AirQualityForecast:UpdateHoursPeriod",
+            updateHoursPeriod.ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:0", _installationIds[0].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:1", _installationIds[1].ToString()),
+        new KeyValuePair<string, string>(
+            "AppSettings:AirlyApi:InstallationIds:2", _installationIds[2].ToString()),
+      };
+
+      var config = new ConfigurationBuilder()
+          .AddInMemoryCollection(configInstallationIds)
+          .Build();
+
+      var airQualityForecastController = new AirQualityForecastController(
+          serviceProvider, config, unitOfWork: _unitOfWork);
+
+      // Act
+      var weatherForecastsList
+          = await airQualityForecastController.DownloadHourlyWeatherForecasts();
+
+      // Assert
+      Assert.Equal(_installationIds.Count, weatherForecastsList.Count);
+      Assert.NotNull(weatherForecastsList[0]);
+      Assert.NotNull(weatherForecastsList[1]);
+      Assert.NotNull(weatherForecastsList[2]);
+    }
+
+    [Fact]
     public async Task downloads_for_all_installations_when_all_air_quality_forecasts_are_out_of_date()
     {
       // Arrange

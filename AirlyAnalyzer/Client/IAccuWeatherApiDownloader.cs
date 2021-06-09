16c628c9f8dfce271a93200c2874812128c204b9
Dawid Sroka <dawid190393@gmail.com>
2021-06-09 23:57:59 +0200

    Add AccuWeather API downloader interface

---
+++ AirlyAnalyzer/Client/IAccuWeatherApiDownloader.cs
@@ -0,0 +1,13 @@
+﻿namespace AirlyAnalyzer.Client
+{
+  using System;
+  using System.Threading.Tasks;
+
+  using AccuWeatherForecast
+    = System.Collections.Generic.List<Models.AccuWeatherForecastObject>;
+
+  public interface IAccuWeatherApiDownloader : IDisposable
+  {
+    public Task<AccuWeatherForecast> DownloadHourlyWeatherForecast(float latitude, float longtitude);
+  }
+}

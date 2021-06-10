e540d9329b35c86030e3a26ae0a2e51fc7c8cd4a
Dawid Sroka <dawid190393@gmail.com>
2021-06-10 23:58:11 +0200

    Fix JS updateInstallationsSelect() to show first installation default

--- AirlyAnalyzer/wwwroot/js/forecastsSite.js
+++ AirlyAnalyzer/wwwroot/js/forecastsSite.js
@@ -66,6 +66,12 @@ function updateInstallationsSelect() {
     option.value = installationId;
     select.add(option);
   }
+
+  if (airQualityForecasts?.length > 0) {
+    if (airQualityForecasts[0].length > 0) {
+      select.value = airQualityForecasts[0][0].InstallationId;
+    }
+  }
 }
 
 function updateForecastChart(selectedInstallationId) {

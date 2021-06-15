9d44675d243330be88623b00fcce377a6d708a1e
Dawid Sroka <dawid190393@gmail.com>
2021-06-15 23:09:17 +0200

    Use ForecastChart class in forecastsSite.js

--- AirlyAnalyzer/wwwroot/js/forecastsSite.js
+++ AirlyAnalyzer/wwwroot/js/forecastsSite.js
@@ -4,12 +4,19 @@ let forecastsDictionary = {}
 let installationAddresses = {};
 let chart = {}
 
+let chartDivClass = "col-12";
+
 forecastDates = createForecastDates();
 installationAddresses = initInstallationAddresses(airQualityForecasts);
 initForecastsDictionary();
 
 $(document).ready(function () {
-  chart = createForecastChart();
+  let firstInstallationId = airQualityForecasts[0][0]?.InstallationId;
+
+  let forecastChart = new ForecastChart(
+    forecastDates, forecastsDictionary[firstInstallationId], chartDivClass);
+
+  chart = forecastChart.createForecastChartDiv();
   updateInstallationsSelect();
 });
 
@@ -47,72 +54,9 @@ function updateInstallationsSelect() {
 function updateForecastChart(selectedInstallationId) {
   if (selectedInstallationId != 0) {
     chart.remove();
-    chart = createForecastChart(selectedInstallationId);
-  }
-}
 
-function createForecastChart(installationId) {
-  let dataIsNotNullOrEmpty = airQualityForecasts != null
-    && airQualityForecasts != undefined
-    && airQualityForecasts.length != 0
-
-  if (dataIsNotNullOrEmpty) {
-    if (installationId == undefined) {
-      installationId = airQualityForecasts[0][0].InstallationId;
-    }
-
-    let yTitle = g => g
-      .append("text")
-      .attr("x", yAxisTitle.x)
-      .attr("y", yAxisTitle.y)
-      .attr("fill", "currentColor")
-      .attr("text-anchor", "start")
-      .attr("font-family", "sans-serif")
-      .attr("font-weight", "bold")
-      .attr("font-size", yAxisTitle.fontSize)
-      .text(yAxisTitle.text);
-
-    let x = d3.scaleBand()
-      .domain(d3.range(forecastDates.length))
-      .range([chartSize.margin.left, chartSize.width - chartSize.margin.right])
-      .padding(0.1);
-
-    let y = d3.scaleLinear()
-      .domain([0, d3.max(forecastsDictionary[installationId], d => d.AirlyCaqi)])
-      .nice()
-      .range([chartSize.height - chartSize.margin.bottom, chartSize.margin.top]);
-
-    let xAxis = g => g
-      .attr("transform", `translate(0,${chartSize.height - chartSize.margin.bottom})`)
-      .call(d3.axisBottom(x)
-        .tickFormat(i => forecastDates[i].getHours())
-        .tickSizeOuter(0));
-
-    let yAxis = g => g
-      .attr("transform", `translate(${chartSize.margin.left},0)`)
-      .call(d3.axisLeft(y))
-      .call(yTitle);
-
-    const svg = d3.select("#mainDiv")
-      .append("svg")
-      .attr("viewBox", [0, 0, chartSize.width, chartSize.height]);
-
-    svg.append("g")
-      .selectAll("rect")
-      .data(forecastsDictionary[installationId])
-      .join("rect")
-      .attr("x", (d, i) => x(i))
-      .attr("y", d => y(d.AirlyCaqi))
-      .attr("height", d => y(0) - y(d.AirlyCaqi))
-      .attr("width", x.bandwidth())
-      .attr("fill", d => getColorForCaqiRange(d.AirlyCaqi));
-
-    svg.append("g")
-      .call(xAxis);
-
-    svg.append("g")
-      .call(yAxis);
-
-    return svg.node();
+    let forecastChart = new ForecastChart(
+      forecastDates, forecastsDictionary[selectedInstallationId], chartDivClass);
+    chart = forecastChart.createForecastChartDiv();
   }
 }
